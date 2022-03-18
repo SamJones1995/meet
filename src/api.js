2ab7dc42cb7cc039017a221fcd5724183ec23d8e
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2022-03-17 23:30:45 -0400

    Placed correct URL in getEvents

--- src/api.js
+++ src/api.js
@@ -64,7 +64,7 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = "https://37ns3waruj.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url" + "/" + token;
+    const url = "https://37ns3waruj.execute-api.us-east-1.amazonaws.com/dev/api/get-events" + "/" + token;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
