b2e96f34e70c68fc457af65875049522b2e6fe49
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2022-04-01 23:15:41 -0400

    Atatus monitor setup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import * as atatus from 'atatus-spa';
 
+
+atatus.config('e8efd7b9e10a4c03abc1229738d11591').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,12 +17,16 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.unregister();
 
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
